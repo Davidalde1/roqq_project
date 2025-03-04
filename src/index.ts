a28d47f225f7cacfb975a4da7b99dd54ee13339d
Davidalde1 <adebayodavid.fruitylife@gmail.com>
2025-03-04 18:58:54 +0100

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,32 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-const app = express();
 import dotenv from 'dotenv';
 import sequelize from './config/database';
-
-
-
-dotenv.config();
-
-//use the exported router
 import userRoutes from './routes/userRoutes';
 import addressRoutes from './routes/addressRoutes';
 import postRoutes from './routes/postRoutes';
 
+dotenv.config();
 
+
+const app = express();
 
 app.use(cors());
-//use the router
 app.use(bodyParser.json());
 
-// Routes
-app.use('/api/user', userRoutes);
-app.use('/api/address', addressRoutes);
-app.use('/api/post', postRoutes);
+//Routes
+app.use('/api/users', userRoutes);
+app.use('/api/addresses', addressRoutes);
+app.use('/api/posts', postRoutes);
 
-
- export default app;
-
-// Database synchronization
+// Database synchronization 
 const PORT = process.env.PORT || 3000;
 
 sequelize.sync().then(() => {
   app.listen(PORT, () => {
-    console.log(`Server is running`);
+    console.log("Server is running");
   });
 }).catch((error) => {
-  console.log(error);
+  console.error(error);
 });
