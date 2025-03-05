bbe797ddab630f1c304829443a793cdea08cd68c
Davidalde1 <adebayodavid.fruitylife@gmail.com>
2025-03-05 09:01:43 +0100

    updated

--- dist/index.js
+++ dist/index.js
@@ -6,26 +6,25 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
+const body_parser_1 = __importDefault(require("body-parser"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const database_1 = __importDefault(require("./config/database"));
 dotenv_1.default.config();
-//use the exported router
-const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
-const addressRoutes_1 = __importDefault(require("./routes/addressRoutes"));
-const postRoutes_1 = __importDefault(require("./routes/postRoutes"));
+const user_1 = __importDefault(require("./routes/user"));
+const address_1 = __importDefault(require("./routes/address"));
+const post_1 = __importDefault(require("./routes/post"));
 app.use((0, cors_1.default)());
-//use the router
-app.use(express_1.default.json());
-// Routes
-app.use('/api/user', userRoutes_1.default);
-app.use('/api/address', addressRoutes_1.default);
-app.use('/api/post', postRoutes_1.default);
-// Database synchronization
+app.use(body_parser_1.default.json());
+//Routes
+app.use('/api/users', user_1.default);
+app.use('/api/addresses', address_1.default);
+app.use('/api/posts', post_1.default);
+// Database synchronization 
 const PORT = process.env.PORT || 3000;
 database_1.default.sync().then(() => {
     app.listen(PORT, () => {
-        console.log(`Server is running`);
+        console.log("Server is running");
     });
 }).catch((error) => {
-    console.log(error);
+    console.error(error);
 });
